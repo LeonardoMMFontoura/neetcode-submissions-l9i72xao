710342ac11deb676081095bddbd694ac789b78a6
Leonardo Mastra <leonardommfontoura@gmail.com>
2026-05-04 23:42:46 -0300

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        gauss = n*(n+1)//2
+        expected = sum(nums)
+        return gauss - expected
